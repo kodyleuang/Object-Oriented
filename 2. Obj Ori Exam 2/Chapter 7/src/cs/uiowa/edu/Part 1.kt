45fc2d8aeaf62276c4b2291ec9722aef348ff8fe
Kody Leuang <kodyleuang@gmail.com>
2018-02-28 11:21:00 -0600

    Added Answers to HW3 & Ch 7 Notes

---
+++ 2. Obj Ori Exam 2/Chapter 7/src/cs/uiowa/edu/Part 1.kt
@@ -0,0 +1,78 @@
+package cs.uiowa.edu
+
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+//Kotlin Puzzles
+/*
+open class A {
+    open fun x() = println("hello")
+}
+class B: A() {
+    override fun x() = println("bye")
+}
+fun main(args: Array<String>) {
+    val a = B()
+    a.x()
+    (a as A).x() // Wut ...
+    val b = A()
+    b.x()
+    (b as B).x() // Why?
+}*/
+
+/*
+//CompareTo operation example
+class Person(val name: String, val Id: Int):
+        Comparable<Person>{
+    override fun hashCode(): Int{
+        return name.hashCode() + Id.hashCode()
+    }
+    override fun equals(other:Any?):Boolean{
+        if(other === this) return true          //Notice "===" means if the objects are pointing to the same thing then it's true
+        if(other !is Person)return false
+        return name == other.name && Id == other.Id
+    }
+    override fun compareTo(other: Person): Int =
+            when {
+                name == other.name && Id == other.Id -> 0
+                name == other.name && Id < other.Id -> -1
+                name < other.name -> -1
+                name == other.name && Id > other.Id -> 1
+                name > other.name -> 1
+                else -> 1
+            }
+
+    override fun toString(): String =
+            "Person[$name,$Id]"
+}
+
+fun main(args: Array<String>) {
+    val p = Person("Gary",23)
+    val q = Person("Diane", 91)
+    val r = Person("Gary",23)
+    val M: Set<Person> = setOf(p,q,r)
+    println(M)
+        //[Person[Gary,23], Person[Diane,91]]
+    val T = listOf(p,r,q).sorted()
+    println(T)
+        //[Person[Diane,91], Person[Gary,23], Person[Gary,23]]
+}*/
+
+
+//Delegation Example
+class PersonRegistry {
+    var M = mutableMapOf<String,Any>()
+    var name: String by M       // makes an element name into M as the <String> half
+    var id: Int by M            // makes the element id into M as the <Any> half
+}
+fun main(args: Array<String>) {
+    val P = PersonRegistry()
+    P.name = "Sven"             //Fills in name in PersonRegistry() class
+    P.id = 3078                 //Fills in id in PersonRegistry() class
+    println(P.M)
+        //{name=Sven, id=3078}
+    P.name = "jin"
+    P.id = 34
+    println(P.M)
+        //{name=jin, id=34}
+        //This means it replaces the original P.M list so it Will Not have more than one element
+}
--- 9. Homework/Homework 3/Homework3/src/edu/uiowa/cs/homework3.kt
+++ 9. Homework/Homework 3/Homework3/src/edu/uiowa/cs/homework3.kt
@@ -1,32 +1,57 @@
 package edu.uiowa.cs
 
-// write code for Disability, Person, Alien, Resident, Adult and Minor
-// classes here (unit test cases are in the ClassTest file)
-// you may need to adjust the run configuration for ClassTest also
-
-enum class Disability{
-    MOBILITY, SIGHT, HEARING, SPEAKING, READING;
+enum class Disability {
+    MOBILITY, SIGHT, HEARING, SPEAKING, READING
 }
 
-open class Person{   //using "Secondary" Constructor syntax
+open class Person {
     val name: String
-    val Id: String
-    var disabilities: MutableSet<Disability> = mutableSetOf<Disability>()
-    constructor(name:String = "", Id: String = "", disabilities: Disability){
-        this.name = name
-        this.Id = Id
+    val id: String
+    var disabilities = setOf<Disability>()
+    constructor(_name: String, _id: String) {
+        name = _name
+        id = _id
+        //Checks to see if the ID is a valid length
+        for (d in id.toCharArray()) {
+            if (d !in "0123456789") throw Error()
+            if (id.length != 9) throw Error()
+        }
+        //Checks for duplicate ID's
+        for (a in PersonList) {
+            if (id == a.id) throw Error()
+        }
+        if (name.isBlank()) throw Error()
+        if (name.trim { it == ' ' } != name) throw Error()
+        PersonList.add(this)
     }
-    //constructor(d:Double): this("bob",3)
+    companion object {
+        val PersonList = mutableListOf<Person>()
+    }
 }
-/*
-open class Person (val name:String, val id:String){
-    var PersonList = mutableListOf<Person>()
+class Alien: Person {
+    val passport: String
+
+    constructor(name: String, id: String, _passport: String)
+            : super(name, id) {
+        passport = _passport
+        if (passport.isBlank()) throw Error()
+    }
 }
-*/
-open class Resident(name:String, id:String):Person(name, id)
-class Adult(name:String, id:String, val disabilities: Disability):Resident(name, id){}
-class Minor(name:String, id:String, val disabilities: Disability):Resident(name, id){}
-
-open class Alien(name:String, id:String,var passport: String):Person(name, id){
-
+open class Resident(name:String,id:String): Person(name,id)
+class Adult(name:String,id:String): Resident(name,id) {
+    var email: String? = null
+    var address: MutableList<String>? = null
+        set(t:MutableList<String>?) {
+            field = t?.toMutableList()
+        }
+    fun setAddress(adr: String) {
+        address = adr.split("\n").toMutableList()
+    }
+}
+class Minor: Resident {
+    var guardian: Person
+    constructor(name:String,id:String,guardian:Person): super(name,id) {
+        this.guardian = guardian
+        if (guardian is Minor) throw Error()
+    }
 }
